cad920811b80580e727692df65e5db2ba97cea8e
Viviana Michel <vmichel@berkeley.edu>
2020-10-13 05:13:20 -0700

    Updates

---
+++ precache-manifest.b772fbf20aa334d7798914ddf64e4330.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f5d5cf74933025419d45e4ee834a45e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "849e3bbf3dcc2bdd9023",
+    "url": "/portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "dcf2e245f349b6cf0d61",
+    "url": "/portfolio/static/css/main.09c8e60b.chunk.css"
+  },
+  {
+    "revision": "849e3bbf3dcc2bdd9023",
+    "url": "/portfolio/static/js/2.f2b07150.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.f2b07150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcf2e245f349b6cf0d61",
+    "url": "/portfolio/static/js/main.6b793d3b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "d78d7aaa17af4674833d85a58bb0adb2",
+    "url": "/portfolio/static/media/enigma.d78d7aaa.jpg"
+  },
+  {
+    "revision": "e4775f43ff48f292eed6f31a0033d0f2",
+    "url": "/portfolio/static/media/gitlet.e4775f43.jpg"
+  },
+  {
+    "revision": "ee65d3704c2f00fb3acafb07a6f16a4f",
+    "url": "/portfolio/static/media/mail.ee65d370.png"
+  },
+  {
+    "revision": "f1f27092732b4ce29aa98e98c23bb9f1",
+    "url": "/portfolio/static/media/maze.f1f27092.PNG"
+  },
+  {
+    "revision": "3334282557ae70278ccee8160b08ba95",
+    "url": "/portfolio/static/media/network.33342825.jpg"
+  },
+  {
+    "revision": "bdd3fda06e3a847e8a35919843203697",
+    "url": "/portfolio/static/media/pacman.bdd3fda0.jpg"
+  },
+  {
+    "revision": "e3dba77dda29e6505704c12926b7530e",
+    "url": "/portfolio/static/media/profile.e3dba77d.jpg"
+  },
+  {
+    "revision": "e0c53f449197819c64816c2b399aca29",
+    "url": "/portfolio/static/media/tweeter.e0c53f44.PNG"
+  }
+]);
